fd122c30ba51b09c23901d87da0326d532d3a6ab
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-06-30 12:57:38 -0500

    adding more data for the html

--- app.js
+++ app.js
@@ -126,14 +126,16 @@ Add a New Project
     });
 };
 
+//remove this function at the end and uncomment the promptUser and the .then s
+
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
